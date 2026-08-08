56dca3745172ceca0703fe9907a7822f249b7863
god-s-only <joshxchris098@gmail.com>
2026-08-08 11:57:24 +0100

    feat(domain): add MeshDevice model and observe-nearby-devices use case

---
+++ domain/src/main/java/com/relayo/domain/model/MeshDevice.kt
@@ -0,0 +1,10 @@
+package com.relayo.domain.model
+
+data class MeshDevice(
+    val id:String,
+    val displayName:String,
+    val hopCount:Int,
+    val signalStrength:Int,
+    val lastSeenEpochMillis:Long,
+    val isDirectNeighbor:Boolean
+)
